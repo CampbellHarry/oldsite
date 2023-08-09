192f8a80cfa55ca5e6c687093cedfd414540fba7
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-08-09 02:28:13 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -21,23 +21,6 @@ textElements2[currentIndex2].style.display = 'inline';
 setInterval(showNextText, 3000);
 
 
-document.addEventListener('contextmenu', (e) => e.preventDefault());
-
-function ctrlShiftKey(e, keyCode) {
-return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
-}
-
-document.onkeydown = (e) => {
-// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
-if (
-event.keyCode === 123 ||
-ctrlShiftKey(e, 'I') ||
-ctrlShiftKey(e, 'J') ||
-ctrlShiftKey(e, 'C') ||
-(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
-)
-return false;
-};
 
 
 
