166cb66948f38ce9468a312246e3541cc27ed78a
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-08-09 12:41:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -79,6 +79,24 @@ function redirectToGitHub4() {
     window.open("https://github.com/CampbellHarry/icons", "_blank");
 }
 
+document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+function ctrlShiftKey(e, keyCode) {
+return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+// Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+if (
+event.keyCode === 123 ||
+ctrlShiftKey(e, 'I') ||
+ctrlShiftKey(e, 'J') ||
+ctrlShiftKey(e, 'C') ||
+(e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+)
+return false;
+};
+
 var date1 = new Date('7/12/2023');
 var date2 = new Date();
 var difference = date2.getTime() - date1.getTime();
