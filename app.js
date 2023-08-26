5c3a9ba01c7c743a2991ae87072d7bc81dc7c81b
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-08-26 19:39:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -85,7 +85,23 @@ function redirectToGitHub4() {
 }
 
 // no peek starts
+document.addEventListener('contextmenu', (e) => e.preventDefault());
 
+function ctrlShiftKey(e, keyCode) {
+return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+}
+
+document.onkeydown = (e) => {
+    // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+    if (
+    keyCode === 123 ||
+    ctrlShiftKey(e, 'I') ||
+    ctrlShiftKey(e, 'J') ||
+    ctrlShiftKey(e, 'C') ||
+    (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+    )
+    return false;
+    };;
 
 
 // no peek ends
