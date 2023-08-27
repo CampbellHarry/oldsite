a78c43f4705a2b5841671e132c7a987c2ec98555
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-08-27 21:20:19 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -94,7 +94,7 @@ return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
 document.onkeydown = (e) => {
     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
     if (
-    keyCode === 123 ||
+    event.keyCode === 123 ||
     ctrlShiftKey(e, 'I') ||
     ctrlShiftKey(e, 'J') ||
     ctrlShiftKey(e, 'C') ||
