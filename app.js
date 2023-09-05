963c87aa5b8ac951d5668fb303ab3a99c2e889c6
Harry Campbell <74868245+CampbellHarry@users.noreply.github.com>
2023-09-05 12:53:22 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -266,3 +266,22 @@ document.getElementById("downloadButton").addEventListener("click", function() {
     }
 });
 //button cv download end
+//what i can do starts
+var isWarning = true;
+
+function updateWarningText() {
+    var warningTextElement = document.getElementById("warningText");
+    var warningHeadElement = document.getElementById("warn");
+    var warnBoxElement = document.querySelector(".warnbox");
+
+    if (isWarning) {
+        warningTextElement.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        warningHeadElement.textContent = "Warning";
+        warnBoxElement.style.borderColor = "#ffcc00";
+    } else {
+        warningTextElement.textContent = "Attention This is a urgent Message";
+        warningHeadElement.textContent = "Urgent Message";
+        warnBoxElement.style.borderColor = "red";
+    }
+    isWarning = !isWarning;
+}
